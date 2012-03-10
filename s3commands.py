5cea3f8aee691e9561bac22323825acb04c69679
David Oxley <git@psi.epsilon.org.uk>
2012-03-10 22:21:19 +0000

    removing Subversion $Id$ lines

--- s3commands.py
+++ s3commands.py
@@ -14,10 +14,6 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-#
-# $Id: s3commands.py 145 2010-05-22 19:48:29Z dgeo2 $
-#
-
 import time
 import socket
 import hmac
